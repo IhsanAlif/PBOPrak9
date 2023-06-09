6d19bf9c8344dd75bda4b2dbfecacf9e6fcab22c
IhsanAlif <100286370+IhsanAlif@users.noreply.github.com>
2023-06-09 17:09:24 +0700

    Create PersonDAO.java

---
+++ PersonDAO.java
@@ -0,0 +1,11 @@
+/*
+* Nama File    : PersonDAO.java
+* Nama Pembuat : Ihsan Alif Lazuardi Anwar
+* NIM          : 24060121140162
+* Deskripsi    : Interface untuk person access object
+* Tanggal      : 06 Juni 2023
+*/
+
+public interface PersonDAO {
+    public void savePerson(Person p) throws Exception;
+}
